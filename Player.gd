fbc2de895fc70067e286833be9da051b3c04ca40
Nathan Eloe <powerofazure@gmail.com>
2023-01-23 10:37:39 -0600

    Finish player anim/signals

--- Player.gd
+++ Player.gd
@@ -1,5 +1,6 @@
 extends Area2D
 
+signal hit
 
 # Declare member variables here. Examples:
 export var speed = 400
@@ -9,7 +10,12 @@ var screen_size
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	screen_size = get_viewport_rect().size
+	hide()
 
+func start(pos):
+	position = pos
+	show()
+	$CollisionShape2D.disabled = false
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
@@ -23,9 +29,26 @@ func _process(delta):
 	if Input.is_action_pressed("move_up"):
 		velocity.y -= 1
 	
+	if velocity.x !=0:
+		$AnimatedSprite.animation = 'walk'
+		$AnimatedSprite.flip_v = false
+		$AnimatedSprite.flip_h = velocity.x < 0
+	elif velocity.y != 0:
+		$AnimatedSprite.animation = 'up'
+		$AnimatedSprite.flip_v = velocity.y > 0
+		$AnimatedSprite.flip_h = false
+	
 	if velocity.length() > 0:
 		velocity = velocity.normalized() * speed
 		$AnimatedSprite.play()
 	else:
 		$AnimatedSprite.stop()
 	position += velocity * delta
+	position.x = clamp(position.x, 0, screen_size.x)
+	position.y = clamp(position.y, 0, screen_size.y)
+
+
+func _on_Player_body_entered(body):
+	hide()
+	emit_signal('hit')
+	$CollisionShape2D.set_deferred("disabled", true)
